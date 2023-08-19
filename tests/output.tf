2fa691bd99b7812d48695c96559568fd9bcc8cf4
Andrey Petrov <a.petrov@aligntech.com>
2023-08-19 12:37:09 +0200

    Add output for tests.

---
+++ tests/output.tf
@@ -0,0 +1,40 @@
+
+output "arn" {
+  value = module.this.arn
+}
+
+output "alias_arn" {
+  value = module.alias_arn
+}
+
+output "alias_name" {
+  value = module.alias_name
+}
+
+output "memory_size" {
+  value = module.this.memory_size
+}
+
+output "reserved_concurrent_executions" {
+  value = module.this.reserved_concurrent_executions
+}
+
+output "ephemeral_storage" {
+  value = module.this.ephemeral_storage
+}
+
+output "architectures" {
+  value = module.this.architectures
+}
+
+output "timeout" {
+  value = module.this.timeout
+}
+
+output "image_uri" {
+  value = module.this.image_uri
+}
+
+output "cloudwatch_log_group_retention_in_days" {
+  value = module.this.cloudwatch_log_group_retention_in_days
+}
