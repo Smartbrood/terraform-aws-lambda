3fd43eb4b53d85e2ac6c8b78d9dbd3f01839cf22
Andrey Petrov <a.petrov@aligntech.com>
2023-08-19 11:37:07 +0200

    Add outputs and variables validations.

--- output.tf
+++ output.tf
@@ -10,3 +10,31 @@ output "alias_arn" {
 output "alias_name" {
   value = aws_lambda_alias.this.name
 }
+
+output "memory_size" {
+  value = aws_lambda_function.this.memory_size
+}
+
+output "reserved_concurrent_executions" {
+  value = aws_lambda_function.this.reserved_concurrent_executions
+}
+
+output "ephemeral_storage" {
+  value = aws_lambda_function.this.ephemeral_storage
+}
+
+output "architectures" {
+  value = aws_lambda_function.this.architectures
+}
+
+output "timeout" {
+  value = aws_lambda_function.this.timeout
+}
+
+output "image_uri" {
+  value = aws_lambda_function.this.image_uri
+}
+
+output "cloudwatch_log_group_retention_in_days" {
+  value = aws_cloudwatch_log_group.this.retention_in_days
+}
